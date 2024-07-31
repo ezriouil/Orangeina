bc0fd7afe514f1b5ba9d8d75d4c2bca0fe721847
ezriouil <ezriouil2003@gmail.com>
2024-07-31 19:51:32 +0100

    Release

--- lib/domain/repositories/report_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/report_entity.dart';
-
-abstract class ReportRepository {
-
-  // - - - - - - - - - - - - - - - - - - NEW REPORT - - - - - - - - - - - - - - - - - -  //
-  Future<void> insert({required ReportEntity reportEntity});
-
-
-}
